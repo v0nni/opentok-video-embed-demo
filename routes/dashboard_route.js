4a0ef997301789347fe3697af7ab5b8809dad1be
v0nni <interactiveheroine@gmail.com>
2019-05-29 11:55:50 -0400

    update

--- routes/dashboard_route.js
+++ routes/dashboard_route.js
@@ -7,16 +7,16 @@ const router = require('express').Router();
  */
 router.get('/coach', (req, res) => {
   res.locals.user = { role: 'Coach' };
-  res.render('dashboard_doctor', { meetings: DB.meetings_filter() })
+  res.render('dashboard_coach', { meetings: DB.meetings_filter() })
 });
 
 /**
  * Patient's dashboard
  */
-router.get('/patient', (req, res) => {
+router.get('/client', (req, res) => {
   res.locals.user = { role: 'client' };
   // Render view only with meetings that were booked
-  res.render('dashboard_patient', { meetings: DB.meetings_filter(true) });
+  res.render('dashboard_client', { meetings: DB.meetings_filter(true) });
 });
 
 module.exports = router;
